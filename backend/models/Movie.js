6a2789233ca57af8ceada586ec6993347570b42f
Paulo Bonifácio <paulo.bonifacio@live.com>
2025-05-18 13:31:05 -0300

    Update Movie.js

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -1,30 +1,50 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
+  movieId: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
   title: {
     type: String,
     required: true,
     trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  releaseDate: {
+    type: Date,
+    required: true,
+  },
+  genre: {
+    type: [String],
+    required: true,
   },
   director: {
     type: String,
     required: true,
     trim: true,
   },
-  releaseYear: {
-    type: Number,
+  cast: {
+    type: [String],
     required: true,
   },
-  genre: {
-    type: String,
+  rating: {
+    type: Number,
     required: true,
-    trim: true,
+    min: 0,
+    max: 10,
   },
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
-  },
+  }
 }, {
   timestamps: true,
 });
